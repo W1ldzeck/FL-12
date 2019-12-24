a4ec974b2702630e0bebc3d1fd4cdd5170859303
wildzeck <wildzeck@gmail.com>
2019-12-24 22:08:42 +0200

    modified js

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,21 +1,24 @@
 let a, b, c;
 
-a = prompt('Enter first value');
-b = prompt('Enter second value');
-c = prompt('Enter third value');
+a = prompt('Enter first value').trim();
+b = prompt('Enter second value').trim();
+c = prompt('Enter third value').trim();
 
 if (!parseInt(a) || !parseInt(b) || !parseInt(c)) {
    alert('input values should be ONLY numbers');
 } else if (a === 0 || b === 0 || c === 0 || Math.sign(a) < 0 || Math.sign(b) < 0 || Math.sign(c) < 0 ) {
     alert('A triangle must have 3 sides with a positive definite length');
 } else {
-    if (a === b && a === c) {
+    a = Number(a);
+    b = Number(b);
+    c = Number(c);
+    if ( a + b <= c || a + c <= b || b + c <= a ) {
+        console.log('Triangle doesn’t exist');
+    } else if (a === c && a === b) {
         console.log('Equilateral triangle');
     } else if (a !== b && b !== c && a !== c) {
         console.log('Scalene triangle');
-    } else if (a === b || b === c || a === c) {
-        console.log('Isosceles triangle')
     } else {
-        console.log('Triangle doesn’t exist');
+        console.log('Isosceles triangle');
     }
 }
